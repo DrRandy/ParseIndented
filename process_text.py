8446a14b90aae89dd719b6178aa2a8e1db5cffe7
DrRandy <DrRandy@users.noreply.github.com>
2024-01-12 23:27:32 -0500

    Update process_text.py

--- process_text.py
+++ process_text.py
@@ -15,13 +15,12 @@ def process_text(input_text):
 
         # If is more indented than previously, create a new sublist
         if indent > current_indent:
-            new_sublist = []
-            current_list.append(new_sublist)
-            stack.append(new_sublist)
+            the_sublist = current_list[-1]['children']
+            stack.append(the_sublist)
             current_indent += 4
 
         current_list = stack[-1]
-        current_list.append(line_content)
+        current_list.append({'section':line_content, 'children':[]})
 
     return result
 
@@ -58,37 +57,44 @@ Neurological
         Lower Extremity Reflexes
             Knee Jerks
             Ankle Jerk Reflexes
-        Special Refles
+        Special Reflexes
     Cerebellar
 Psychiatric"""
         
-print(process_text(input_text))
+output = [{'section': '', 'children': []}, 
+{'section': 'Constitutional', 'children': [
+    {'section': 'General', 'children': []}, 
+    {'section': 'Vitals', 'children': []}]}, 
+{'section': 'HEENT', 'children': [
+    {'section': 'Head', 'children': []}, 
+    {'section': 'Eyes', 'children': []}, 
+    {'section': 'Ears', 'children': []}, 
+    {'section': 'Nose', 'children': []}, 
+    {'section': 'Throat', 'children': []}]}, 
+{'section': 'Pulmonary', 'children': []}, 
+{'section': 'Cardiovascular', 'children': []}, 
+{'section': 'Gastrointestinal', 'children': [
+    {'section': 'Inspection', 'children': []}, 
+    {'section': 'Auscultation', 'children': []}, 
+    {'section': 'Palpation', 'children': []}]}, 
+{'section': 'Genitourinary', 'children': []}, 
+{'section': 'Dermatologic', 'children': []}, 
+{'section': 'Musculoskeletal', 'children': []}, 
+{'section': 'Neurological', 'children': [
+    {'section': 'Cranial Nerves', 'children': []}, 
+    {'section': 'Mental Status (Neuro)', 'children': []}, 
+    {'section': 'Sensory', 'children': []}, {'section': 'Motor', 'children': []}, 
+    {'section': 'Reflexes', 'children': [
+        {'section': 'Upper Extremity Reflexes', 'children': [
+            {'section': 'Biceps', 'children': []}, 
+            {'section': 'Brachioradialis', 'children': []}, 
+            {'section': 'Triceps', 'children': []}]}, 
+        {'section': 'Lower Extremity Reflexes', 'children': [
+            {'section': 'Knee Jerks', 'children': []}, 
+            {'section': 'Ankle Jerk Reflexes', 'children': []}]}, 
+        {'section': 'Special Reflexes', 'children': []}]}, 
+    {'section': 'Cerebellar', 'children': []}]}, 
+{'section': 'Psychiatric', 'children': []}]
 
-
-['', 
-'Constitutional', 
-[   'General', 
-    'Vitals'], 
-'HEENT', 
-[   'Head', 
-    'Eyes', 
-    'Ears', 
-    'Nose', 
-    'Throat'], 
-'Pulmonary', 
-'Cardiovascular', 
-'Gastrointestinal', 
-[   'Inspection', 
-    'Auscultation',     
-    'Palpation'], 
-'Genitourinary', '  
-'Dermatologic', 
-'Musculoskeletal', 
-'Neurological', 
-[   'Cranial Nerves', 
-    'Mental Status (Neuro)', 
-    'Sensory', 
-    'Motor', 
-    'Reflexes', 
-    'Cerebellar'], 
-    'Psychiatric']
+result = process_text(input_text)
+print(result == output)
